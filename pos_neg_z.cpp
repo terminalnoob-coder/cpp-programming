e1f856a81d222b2037848440c77e4758c37386dd
terminalnoob-coder <emailunknown011@gmail.com>
2025-06-23 18:01:51 +0530

    added sign checker

---
+++ pos_neg_z.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  cout<<"Enter a number: ";
+  cin>>a;
+  if(a>0)
+    cout<<"Positive";
+  else if(a<0)
+    cout<<"Negative";
+  else
+    cout<<"Zero";
+  return 0;
+}
